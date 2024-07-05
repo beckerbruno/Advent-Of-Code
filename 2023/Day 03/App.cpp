3b67dcd7341d5828f75243cb825aa149dc492d6c
beckerbruno <bruno.beckersilva@gmail.com>
2024-07-05 20:17:13 -0300

    day03

--- 2023/Day 03/App.cpp
+++ 2023/Day 03/App.cpp
@@ -6,54 +6,24 @@
 #include <string>   
 #include <vector>   
 #include <cctype>   // isdigit()
+#include <map>
 
 #define DEBUG
 
 using namespace std;
-
-#include <iostream>
-#include <string>
-
-std::string extrairNumero(const std::string& str, int i) {
-    // Encontrar o índice do início do número
-    size_t start_index = 0;
-    for (int count = 0; count < i; ++count) {
-        start_index = str.find('.', start_index) + 1;
-    }
-
-    // Encontrar o índice do fim do número
-    size_t end_index = str.find('.', start_index);
-
-    // Extrair o número da string e retorná-lo
-    std::string numero = str.substr(start_index, end_index - start_index);
-    return numero;
-}
-
-string linhaParaString(const vector<vector<char>>& matriz, int linha) {
-    string resultado;
-    for (char c : matriz[linha]) {
-        resultado += c;
-    }
-    return resultado;
-}
-
-// int main() {
-//     std::string string_exemplo = "...123..45..89";
-//     int i = 10;
-//     std::string numero = extrairNumero(string_exemplo, i);
-//     std::cout << "Número: " << numero << std::endl;
-//     return 0;
-// }
-
 
 int main() {
     ifstream arq;        // Cria input file stream (ifstream)
     string line;
     string numero;
     vector<vector<char>> MatrizAdj;
+    vector<vector<int>> positions;
     vector<int> output;
     vector<int>numeros;
     bool isValid = 0;
+    std::map <std::pair<int, int>, char> simb2;
+    std::map <std::pair<int, int>, std::string> matriz;
+    int size_line = 0;
 
     arq.open("InputP1.txt", ios::in); // Abre arquivo
 
@@ -65,12 +35,12 @@ int main() {
     //--------------------------------------------------------------
     //                    CRIANDO A MATRIZ
     //--------------------------------------------------------------
-
+    int jaux = 0;
     while (getline(arq, line)) {    // enquanto houver linhas no arquivo
         #ifdef DEBUG
-            cout << "\nChamando a linha --| " << line << " |--| ";
+            cout << "\nChamando a linha " << jaux << " --| " << line << " |--| ";
         #endif
-
+        string number = "";
         vector<char> linhaMatriz;
         for (int i = 0; i < line.length(); i++) {
             char caractere = line[i];
@@ -78,9 +48,126 @@ int main() {
             #ifdef DEBUG
                 cout << linhaMatriz[i] << " ";
             #endif
+            if (isdigit(caractere)){                          //  se for um numero, adiciona a posicao e o caractere
+                positions.push_back({i, jaux});
+                number = number + caractere;
+            }
+            if (!isdigit(caractere) && number != ""){         //  se nao for um numero, mas tiver algo nos numeros, preenche os valores de map
+                for (const auto& position : positions) {
+                    matriz[{position[0], position[1]}] = number;
+                }
+                number.clear();
+                positions.clear();
+            }
+            if (!isdigit(caractere) && caractere != '.'){     // se for um symbol, poe em simbolo
+                simb2[{i, jaux}] = caractere;
+            }
         }
         MatrizAdj.push_back(linhaMatriz);
+        jaux ++;
     }
+    size_line = line.size();
+
+    //--------------------------------------------------------------
+    //              PRINTANDO A MAPA E simbolo
+    //--------------------------------------------------------------
+
+    cout << "\nMatrix:" << endl;
+    for (const auto& pair : matriz) {
+        cout << pair.first.first << ", " << pair.first.second << ": " << pair.second << endl;
+    }
+
+    cout << "\nsimbolo:" << endl;
+    for (const auto& pair : simb2) {
+        cout << pair.first.first << ", " << pair.first.second << ": " << pair.second << endl;
+    }
+
+    
+
+    for (auto& pair : matriz) {
+        int i = pair.first.first; // Obter a primeira coordenada do par
+        int j = pair.first.second; // Obter a segunda coordenada do par
+        string num = pair.second; // Obter o caractere associado ao par
+        
+        #ifdef DEBUG
+            cout << " " << num;
+        #endif
+        // numero += num;
+        // numero.clear();
+        
+        //--------------------------------------------------------------    .1   .2   .3
+        //          VERIFICANDO SE O DIGITO É VALIDO                        .4   ij   .6
+        //--------------------------------------------------------------    .7   .8   .9
+        
+        //------------------------------------ LINHA 1 ^ ---------------
+        if ( i > 0 && j > 0 ) { 
+            // cout << ".1";
+            if (MatrizAdj[i-1][j-1] != '.' && !isdigit(MatrizAdj[i-1][j-1]) ){
+                isValid = 1;
+            }
+        }
+        if ( i > 0 ) { 
+            // cout << ".2";
+            if (MatrizAdj[i-1][j]   != '.' && !isdigit(MatrizAdj[i-1][j]) ){
+                isValid = 1;
+            }
+        }
+        if ( i > 0 && j < MatrizAdj[i].size() - 1 ) { 
+            // cout << ".3";
+            if (MatrizAdj[i-1][j+1] != '.' && !isdigit(MatrizAdj[i-1][j+1]) ){
+                isValid = 1;
+            }
+        }
+
+        //------------------------------------ LINHA 2 + ---------------
+        if ( j > 0 ) { 
+            // cout << ".4";
+            if ( MatrizAdj[i][j-1] != '.' && !isdigit(MatrizAdj[i][j-1]) ){
+                isValid = 1;
+            }
+        }
+        if ( j < MatrizAdj[i].size() - 1 ) { 
+            // cout << ".6";
+            if ( MatrizAdj[i][j+1] != '.' && !isdigit(MatrizAdj[i][j+1]) ){
+                isValid = 1;
+            }
+        }
+        
+        //------------------------------------ LINHA 3 _ ---------------
+        if ( i < MatrizAdj.size() - 1 && j > 0 ) { 
+            // cout << ".7";
+            if ( MatrizAdj[i+1][j-1] != '.' && !isdigit(MatrizAdj[i+1][j-1]) ){
+                isValid = 1;
+            }
+        }
+        if ( i < MatrizAdj.size() - 1 ) { 
+            // cout << ".8";
+            if ( MatrizAdj[i+1][j]   != '.' && !isdigit(MatrizAdj[i+1][j]) ){
+                isValid = 1;
+            }
+        }
+        if ( i < MatrizAdj.size() - 1 && j < MatrizAdj[i].size() - 1 ) { 
+            // cout << ".9";
+            if ( MatrizAdj[i+1][j+1] != '.' && !isdigit(MatrizAdj[i+1][j+1]) ){
+                isValid = 1;
+            }
+        }
+
+        //--------------------------------------------------------------
+        //          ADICIONANDO NUMERO COMPLETO A SAÍDA
+        //--------------------------------------------------------------
+        
+        if ( isValid ){
+            int aux = MatrizAdj[i][j] - '0';
+            // string linha_string = linhaParaString(MatrizAdj, i);
+            // cout << ".V:" << extrairNumero(linha_string, j) << " ";
+            output.push_back(aux);
+        }
+        isValid = 0;
+    }
+
+
+
 
     //--------------------------------------------------------------
     //                    ACESSANDO A MATRIZ
@@ -90,7 +177,7 @@ int main() {
             cout << endl;
         #endif
         for ( int j = 0; j < MatrizAdj[i].size(); j++) {         // j para cada coluna
-            if( MatrizAdj[i][j] != '.' && isdigit(MatrizAdj[i][j]) ){
+            if( isdigit(MatrizAdj[i][j]) ){                      // se é numero
                 #ifdef DEBUG
                     cout << " " << MatrizAdj[i][j];
                 #endif
@@ -162,8 +249,8 @@ int main() {
                 
                 if ( isValid ){
                     int aux = MatrizAdj[i][j] - '0';
-                    string linha_string = linhaParaString(MatrizAdj, i);
-                    cout << ".V:" << extrairNumero(linha_string, j) << " ";
+                    // string linha_string = linhaParaString(MatrizAdj, i);
+                    // cout << ".V:" << extrairNumero(linha_string, j) << " ";
                     output.push_back(aux);
                 }
                 isValid = 0;
@@ -182,3 +269,167 @@ int main() {
     arq.close();
     return 0;
 }
+
+/*
+python
+input = """467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."""
+
+numbers = ['1','2','3','4','5','6','7','8','9','0']
+
+def near(x, y):
+  return [
+      [x -1, y -1], [x, y - 1], [x + 1, y - 1],
+      [x -1, y   ],             [x + 1, y    ],
+      [x -1, y +1], [x, y + 1], [x + 1, y + 1]
+    ]
+
+def is_number_str(number_str):
+    return number_str in numbers
+    
+def is_symbol(c):
+    return c not in numbers and c != '.'
+    
+def vector_get_number_positions_and_symbol_positions(input):
+    lines = input.split("\n")
+    vector_number_and_positions = {}
+    vector_symbol_positions = []
+    number = ''
+    vector_current_number_positions = []
+    for y, line in enumerate(lines):
+        for x, c in enumerate(line):
+            if is_number_str(c):
+                vector_current_number_positions .append([x,y])
+                number = number + c
+            if  not is_number_str(c) and number != '':
+                vector_number_and_positions[int(number)] = vector_current_number_positions
+                number = ''
+                vector_current_number_positions  = []
+            if is_symbol(c):
+                vector_symbol_positions.append([x,y])
+    return vector_number_and_positions, vector_symbol_positions
+
+def get_symbols_surrounding_zone(vector_symbol_positions):
+    surrounding_zone = []
+    for position in vector_symbol_positions:
+        x, y = position
+        surrounding_zone = surrounding_zone + near(x, y)
+    return surrounding_zone
+
+def get_numbers_with_symbols_in_surroundings(vector_number_and_positions, symbols_surrounding_zone):
+    numbers_with_symbols_near = []
+    for number, positions in vector_number_and_positions.items():
+        any_symbol_near = False
+        for position in positions:
+            if position in symbols_surrounding_zone:
+                any_symbol_near = True
+        if any_symbol_near:
+            numbers_with_symbols_near .append(number)
+    return numbers_with_symbols_near
+
+if __name__ == "__main__":
+    vector_number_and_positions, vector_symbol_positions = vector_get_number_positions_and_symbol_positions(input)
+    surrounding_zone = get_symbols_surrounding_zone(vector_symbol_positions)
+    numbers_with_symbols_near = get_numbers_with_symbols_in_surroundings(vector_number_and_positions, surrounding_zone)
+    sum = sum(numbers_with_symbols_near)
+    print(sum)
+
+*/
+
+
+
+
+/*
+python
+input = """
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..""" 
+
+# se divide na nova linha para ter uma matriz
+lines = input.split("\n")
+
+# numeros e as posicoes da matriz que ocupam
+map = {}
+# simbolos e as posicoes da matriz que ocupam
+simbolo = {}
+
+# numero atual pra montar os numeros ao invez de mostrar so um por casa
+number = ''
+# as posicoes que o numero atual ocupa, pra gente preencher depois
+positions = []
+# o que sao os numeros, tem jeito melhor de fazer isso... mas ta bueno vai assim
+numbers = ['1','2','3','4','5','6','7','8','9','0']
+
+# pra cada linha
+for i, line in enumerate(lines):
+    # pra cada caractere
+    for j, c in enumerate(line):
+        # se for um numero, adiciona a posicao e o caractere
+        if c in numbers:
+            positions.append([i,j])
+            number = number + c 
+
+        # se nao for um numero, mas tiver algo nos numeros, preenche os valores
+        # de map
+        if c not in numbers and number != '':
+            for position in positions:
+                map[str(position)] = number
+            number = ''
+            positions = []
+
+        # se for um symbol, poe em simbolo
+        if c not in numbers and c != '.':
+            simbolo[str([i,j])] = c
+
+
+print(simbolo)
+print(map)
+
+#o que tem em volta da posicao x,y
+def near(j, i):
+    return [
+     [j -1, i -1], [j, i - 1] , [j + 1, i - 1],
+     [j -1, i]               ,  [j + 1, i],
+     [j -1, i +1], [j, i + 1],  [j +1, i +1]] 
+
+
+# quais zonas estao ao arredor de um symbol
+zone = []
+for key, operation in simbolo.items():
+    i, j = eval(key)
+    zone = zone + near(i, j)
+
+print(zone)
+
+# aqui pode dar bug, mas sao todos os numeros que tem symbol em volta
+# se tiver mais de um numero igual pode dar ruim por cause do set que torna
+# unico os valores...
+uniques = [] 
+
+for key, value in map.items():
+    key_list = eval(key)
+    # se a chave ta na zona de arredores de algum symbol, poe na lista
+    if key_list in zone:
+        uniques.append(int(value))
+
+
+# pra cada numero unique soma
+sum = 0
+for u in set(uniques):
+*/
